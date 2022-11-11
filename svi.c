5ed276650a8212572ac267377cf796bd883b10d4
Spencer Gannon <spencer.gannon@valpo.edu>
2022-11-11 12:42:14 -0600

    Current state of the project

--- svi.c
+++ svi.c
@@ -1,244 +1,228 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <string.h>
 
-#define STR_MAX 256
-#define COMM_MAX 100
+#define MAXCHAR 256 //max number of characters per 256
+#define MAXTEXT 81 // max number lines
+#define MAXCOMM 100 // max number of commands in file
 
-typedef enum {A, I, O, d, s} COMM_OPERATIONS;
+typedef enum {none, text, line} linespecification;
+typedef char str[MAXCHAR];
 
-typedef union {
-	int all;
-	char str[STR_MAX];
-	int range[2];
-} lrange;
+typedef union {   // given text or a range of lines (backslash or number)
+        int none;
+        str contained_text;
+        int linenumbers[2];
+} lineSpecRange;
 
 typedef struct {
-	unsigned int lrs: 2;	//0 text, 1 ints, 2 all
-	lrange lr;
-	COMM_OPERATIONS op: 3;
-	char data[STR_MAX];
-} edit_command;
+        char op;
+        linespecification SpecType;
+        lineSpecRange Range;
+        str text;
 
-int determine_op(char op_char){
-	switch(op_char){
-		case 'A':
-			return A;
-			break;
-		case 'I':
-			return I;
-			break;
-		case 'O':
-			return O;
-			break;
-		case 'd':
-			return d;
-			break;
-		case 's':
-			return s;
-			break;
-		default:
-			fprintf(stderr, "[ERROR] operation type could not be determined\n");
-			exit(EXIT_FAILURE);
-			return 0;
+} editStruct;
+
+typedef editStruct editArray[MAXCOMM];
+
+// int isdigit(char ch){
+// 	char arr[10] = {'1','2','3','4','5','6','7','8','9','0'};
+// 	for(int i = 0; i < 10; i++){
+// 		if(ch == arr[i]){
+// 			return 1;
+// 		}
+// 	}
+// 	return 0;
+// }
+
+editStruct TexttoEdit (str command){
+
+	if(command[strlen(command)-1] == '\n'){
+		command[strlen(command)-1] = "\0";
 	}
+
+	editStruct editStruct;
+	char *term;
+
+	if(command[0] == '/') {
+		editStruct.SpecType = text;
+		command = command + 1;
+		term= strchr(command,'/');
+		*term = '\0';
+		strcpy(editStruct.Range.contained_text,command);
+		command = term +1;
+		editStruct.op = command[0];
+		strncpy(editStruct.text, command + 1, strlen(command));
+
+	}
+	else if(isdigit(command[0])){
+		editStruct.SpecType= line;
+		editStruct.Range.linenumbers[0]= atoi(command);
+		command = command +2;
+		editStruct.Range.linenumbers[1]= atoi(command);
+		term = strchr(command,'/');
+		command = term +1;
+		editStruct.op = command[0];
+		strncpy(editStruct.text, command +1, strlen(command));
+	}
+	else{
+		editStruct.SpecType = none;
+		editStruct.Range.none = 1;
+		editStruct.op = command[0];
+		strncpy(editStruct.text, command +2, strlen(command));
+	}
+
+	return(editStruct);
 }
 
-int main(int argc, char** argv){
-	int num_commands = 0;
-	int num_input_lines = 0;
-	int num_output_lines = 0;
-	char svi_input[COMM_MAX][STR_MAX];
-	char svi_output[COMM_MAX][STR_MAX];
-	char buffer[STR_MAX];
-	FILE* file;
 
-	// if(argc != 2 && argc != 3){
-	// 	fprintf(stderr, "[ERROR] Too %s inputs\n", 
-	// 		(argc < 2 ? "few" : "many"));
-	// 	exit(EXIT_FAILURE);
-	// }
-	// file = fopen(argv[1], "r");
+int readInput(FILE* file, editArray *edits) {
+	int i = 0;
+	char inputLine;
 
-	edit_command commands[COMM_MAX];
-
-	fgets(buffer, STR_MAX, stdin);
-	while(strncmp("\n", buffer, STR_MAX)){
-		char range[STR_MAX];
-		char* end;
-
-		edit_command ecom;
-
-		int single_char = 1;
-		int digit_char =  0;
-		int slash_char =  0;
-
-		switch(buffer[0]){
-			case 'A':
-			case 'I':
-			case 'O':
-			case 's':
-			case 'd':
-				break;
-			default:
-				single_char = 0;
-				break;
-		};
-		for(int i = 48; i < 58; i++){
-			if(buffer[0] == i){
-				digit_char = 1;
-			}
-		}
-		if(buffer[0] == '/'){
-			slash_char = 1;
-		}
-
-		if(!digit_char && !slash_char && !single_char){
-			fprintf(stderr, "[ERROR] invalid input\n");
-			exit(EXIT_FAILURE);
-		}
-
-		if(single_char){
-			ecom.lrs = 2;
-			ecom.lr.all = 1;
-			ecom.op = determine_op(buffer[0]);
-		}
-		else if(slash_char){
-			char* end = strstr(buffer + 1, "/");
-			char* point = buffer + 1;
-
-			for(int i = 0; point != end; point++, i++){
-				range[i] = *point;
-			}
-			printf("Our text range is: %s\n", range);
-			//segfaults around here
-			strncpy(ecom.lr.str, range, STR_MAX);
-			ecom.lrs = 0;
-			ecom.op = determine_op(*(end + 1));
-		}
-		else if(digit_char){
-			char* end = strstr(buffer, "/");
-			char* point = buffer;
-
-			for(int i = 0; point != end; point++, i++){
-				range[i] = *point;
-			}
-			printf("Our int range is: %s\n", range);
-			
-			int switch_ints = 0;
-
-			int first_int_size = 0;
-			char first_int[10];
-
-			int second_int_size = 0;
-			char second_int[10];
-			for(int i = 0; i < strlen(range); i++){
-				if(range[i] != ',' && !switch_ints){
-					first_int[first_int_size] = range[i];
-					first_int_size++;
-				}
-				else if(range[i] == ','){
-					switch_ints = 1;
-				}
-				else if(range[i] != ',' && switch_ints){
-					second_int[second_int_size] = range[i];
-					second_int_size++;
-				}
-			}
-			printf("%s %s\n", first_int, second_int);
-			ecom.lrs = 1;
-			ecom.lr.range[0] = atoi(first_int);
-			ecom.lr.range[1] = atoi(second_int);
-			ecom.op = determine_op(*(end + 1));
-		}
-		else{
-			fprintf(stderr, "[ERROR] Invalid input encountered\n");
-			exit(EXIT_FAILURE);
-		}
-
-		if(ecom.op == A || ecom.op == I || ecom.op == O){
-			if(ecom.lrs != 2)
-				end++;
-			else
-				end = buffer + 1;
-			char text[STR_MAX];
-			for(int i = 0; *end != '\n'; end++, i++){
-				text[i] = *end;
-			}
-			printf("Our text given is %s\n", text);
-		}
-		else if(ecom.op == s){
-			end += 2;	//points after the s/ 
-			char* thrd_slash = strstr(end, "/");
-			char old_text[STR_MAX];
-			char new_text[STR_MAX];
-
-			for(int i = 0; end != thrd_slash; end++, i++){
-				old_text[i] = *end;
-			}
-
-			end++;	//after s/old_text/
-			char* frth_slash = strstr(end, "/");
-
-			for(int i = 0; end != frth_slash; end++, i++){
-				new_text[i] = *end;
-			}
-
-			printf("Our old text is %s\n", old_text);
-			printf("Our new text is %s\n", new_text);
-		}
-		else if(ecom.op == d){
-			//todo
-		}
-		else{
-			fprintf(stderr, "[ERROR] Encountered invalid operation\n");
-			exit(EXIT_FAILURE);
-		}
-		commands[num_commands++] = ecom;
-
-		for(int i = 0; i < STR_MAX; i++){
-			buffer[i] = ' ';
-		}
-		fgets(buffer, STR_MAX, stdin);
+	while(fgets(&inputLine, MAXCHAR, file) && (i < MAXCOMM)){
+		*edits[i] = TexttoEdit(&inputLine);
+		i++;
 	}
-	//get the text and perform the commands
-	if(argc == 3){
-		FILE* input_file = fopen(argv[2], "r");
-		char* buffer;
-		while(fgets(buffer, STR_MAX, input_file) != NULL){
-			strncpy(svi_input[num_input_lines], buffer, STR_MAX);
-			num_input_lines++;
+	return i;
+}
+
+
+str* DoEdit(str currentline, editStruct editCmd){
+	int perform_op = 0;
+	str tempString;
+
+	if(editCmd.SpecType == none){
+		perform_op = 1;
+	}
+	else if(editCmd.SpecType == text){
+		if(strstr(currentline, editCmd.Range.contained_text)){
+			perform_op = 1;
+		}
+	}
+	else if(editCmd.SpecType == line){
+		char* space = currentline;
+		str line_num;
+		for(int i = 0; (i < MAXTEXT) && (currentline[i] != ' '); i++){
+			line_num[i] = currentline[i];
+			line_num[i + 1] = '\0';
+			space++;
+		}
+		if(atoi(line_num) >= editCmd.Range.linenumbers[0] && 
+			atoi(line_num) <= editCmd.Range.linenumbers[1]){
+			perform_op = 1;
+		}
+	}
+
+	if(perform_op){
+		switch(editCmd.op){
+			case 'A':
+			{
+				strcat(currentline, editCmd.text);
+				break;
+			}
+			case 'I':
+			{
+				strcpy(tempString, editCmd.text);
+				strcat(tempString, currentline);
+				strcpy(currentline, tempString);
+				break;
+			}
+			case 'O':
+			{
+				printf("%s\n",editCmd.text);
+				break;
+			}
+			case 's':
+			{
+				str delim, copy_edit_dat;
+				char *search, *replace, *pos;
+
+				delim[0] = editCmd.text[0];
+				delim[1] = '\0';
+				strcpy(copy_edit_dat, &editCmd.text[1]);
+				search = strtok(copy_edit_dat,delim);
+				replace = strtok(NULL,delim);
+				
+				while ((pos = strstr(currentline, search)) != NULL) {
+					strncpy(tempString, currentline, pos - currentline);
+					tempString[pos - currentline] = '\0';
+					strcat(tempString, replace);
+					strcat(tempString, &currentline[(pos - currentline) + 
+	strlen(search)]);
+					strcpy(currentline, tempString);
+				}
+
+				// str* cmd = editCmd.text;
+				
+				// char *second_slash = strchr(cmd, '/');
+				// str oldtext;
+
+				// strcpy(oldtext, cmd);
+				// cmd = second_slash + 1;
+				// char* third_slash =strchr(cmd,'/');
+				// *third_slash= '\0';
+				// str newtext;
+				// strcpy(newtext, cmd);
+				// str* subStr = strstr(currentline, oldtext);
+
+				// if(subStr != NULL) {
+				// 	strncpy(tempString, currentline, subStr - currentline);
+				// 	tempString[subStr - currentline] = '\0';
+				// 	strcat(tempString, newtext);
+				// 	strcat(tempString, &currentline[(subStr - currentline) + strlen(oldtext)];
+				// 	strcpy(currentline,tempString);
+				// }
+				break;
+			}
+			case 'd':
+				{
+					strcpy(tempString, "");
+					break;
+				}
+			default:
+				{
+					fprintf(stderr, "[ERROR] Operation type could not be determined\n");
+					exit(EXIT_FAILURE);
+					break;
+				}
 		}
 	}
 	else{
-		//user input
+		strncpy(tempString, currentline, MAXTEXT);
+	}
+	return &tempString;
+}
+                
+int main (int argc, char** argv) {
+	FILE *commandlist; //Where the commands are stored
+	str currentline;
+	editArray edits;
+
+	if(argc != 2) {
+		fprintf(stderr, "[ERROR] Invalid input\n");
+		return EXIT_FAILURE;
 	}
 
-	for(int i = 0; i < num_input_lines; i++){
-		for(int j = 0; j < num_commands; j++){
-			if(commands[j].lrs == 2){
-				//performs command on all text
-			}
-			else if(commands[j].lrs == 1){
-				if(i + 1 >= commands[j].lr.range[0] && i + 1 <= commands[j].lr.range[1]){
-					//if its in the range
-				}
-			}
-			else if(commands[j].lrs == 0){
-				if(strstr(svi_input[i], commands[j].lr.str) != NULL){
-					//if it exists in the input line
-				}
-			}
-			else{
-				fprintf(stderr, "[ERROR] Ran into an invalid lrs\n");
-			}
+	commandlist = fopen(argv[1], 'r');
+
+	if(commandlist == NULL) {
+		fprintf(stderr, "[ERROR] File doesn't exist\n");
+		return EXIT_FAILURE;
+	}
+
+	int size = readInput(commandlist, &edits);
+	fclose(commandlist);
+
+	str output[1024];
+	int output_size = 0;
+	while(fgets(currentline, MAXCHAR, stdin)){
+		strcpy(output[output_size++], currentline);
+		for(int x = 0; x < size; x++) {
+			strcpy(output[output_size], 
+				DoEdit(output[output_size], edits[x]));
 		}
 	}
-
-	for(int i = 0; i < num_output_lines; i++){
-		printf("%s\n", svi_output[i]);
-	}
-
-	exit(EXIT_SUCCESS);
+	
+	return EXIT_SUCCESS;
 }
